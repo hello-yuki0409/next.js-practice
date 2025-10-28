6d34d5e9c8bf7b698b962c6984b6e3683eff07f7
hello-yuki0409 <bobu2784@gmail.com>
2025-10-28 22:45:19 +0900

    test: api叩く

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,66 +1,37 @@
+import { Article } from "@/src/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article>
-        <Link href="#">
-          <Image src="https://picsum.photos/800" alt="" width={1280} height={300} />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">
-            Technology
+      {articles.map((article) => (
+        <article className="shadow my-4 flex flex-col" key={article.id}>
+          <Link href="#">
+            <Image src="https://picsum.photos/800" alt="" width={1280} height={300} />
           </Link>
-          <Link href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">
-            Lorem Ipsum Dolor Sit Amet Dolor Sit Amet
-          </Link>
-          <p className="text-sm pb-3">
-            By{" "}
-            <Link href="#" className="font-semibold hover:text-gray-800">
-              David Grzyb
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">
+              Technology
             </Link>
-            , Published on April 25th, 2020
-          </p>
-          <Link href="#" className="pb-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu
-            iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet
-            posuere magna..
-          </Link>
-          <Link href="#" className="uppercase text-gray-800 hover:text-black">
-            Continue Reading
-          </Link>
-        </div>
-      </article>
-      <article>
-        <Link href="#">
-          <Image src="https://picsum.photos/800" alt="" width={1280} height={300} />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">
-            Technology
-          </Link>
-          <Link href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">
-            Lorem Ipsum Dolor Sit Amet Dolor Sit Amet
-          </Link>
-          <p className="text-sm pb-3">
-            By{" "}
-            <Link href="#" className="font-semibold hover:text-gray-800">
-              David Grzyb
+            <Link href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">
+              {article.title}
             </Link>
-            , Published on April 25th, 2020
-          </p>
-          <Link href="#" className="pb-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu
-            iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet
-            posuere magna..
-          </Link>
-          <Link href="#" className="uppercase text-gray-800 hover:text-black">
-            Continue Reading
-          </Link>
-        </div>
-      </article>
+            <p className="text-sm pb-3">Published on {article.createdAt}</p>
+            <Link href="#" className="pb-6">
+              {article.content}
+            </Link>
+            <Link href="#" className="uppercase text-gray-800 hover:text-black">
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -2,4 +2,7 @@ import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" }); // 更新が多いのでSSR
+
+  const articles = await res.json(); // データを文字列化
+  return articles;
 };
