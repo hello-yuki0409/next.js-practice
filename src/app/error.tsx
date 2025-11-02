643819223cd3291535734a2c6a74a3e104c73f08
hello-yuki0409 <bobu2784@gmail.com>
2025-11-02 22:18:23 +0900

    feat: エラーページ実装

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,18 @@
 "use client";
-
 import React from "react";
 
-const Error = () => {
-  return <div>Error</div>;
+const Error = ({ reset }: { reset: () => void }) => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h3 className="text-2xl font-bold mb-4">エラーが発生しました。再度お試しください。</h3>
+      <button
+        onClick={() => reset()}
+        className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-500 transition duration-200"
+      >
+        もう一度試す
+      </button>
+    </div>
+  );
 };
 
 export default Error;
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,10 +1,10 @@
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
-  const res = await fetch("http://127.0.0.1:3001/posts", { cache: "no-store" });
+  const res = await fetch("http://127.0.0.1:3001/posts", { cache: "no-store" }); // 更新が多いのでSSR
 
   if (!res.ok) {
-    throw new Error(`HTTP ${res.status}`);
+    throw new Error("エラーが発生しました。");
   }
   const articles: Article[] = await res.json();
   return articles;
