aa70c59d5673da252e22095ef266020b40784a59
hello-yuki0409 <bobu2784@gmail.com>
2025-11-07 23:42:25 +0900

    feat: 文字数制限とコンポーネント

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -2,6 +2,7 @@ import { Article } from "@/src/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import ArticleCard from "./ArticleCard";
 
 type ArticleListProps = {
   articles: Article[];
@@ -11,26 +12,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
       {articles.map((article) => (
-        <article className="shadow my-4 flex flex-col" key={article.id}>
-          <Link href="#">
-            <Image src="https://picsum.photos/800" alt="" width={1280} height={300} />
-          </Link>
-          <div className="bg-white flex flex-col justify-start p-6">
-            <Link href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">
-              Technology
-            </Link>
-            <Link href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">
-              {article.title}
-            </Link>
-            <p className="text-sm pb-3">Published on {article.createdAt}</p>
-            <Link href="#" className="pb-6">
-              {article.content}
-            </Link>
-            <Link href="#" className="uppercase text-gray-800 hover:text-black">
-              続きを読む
-            </Link>
-          </div>
-        </article>
+        <ArticleCard article={article} key={article.id} />
       ))}
     </div>
   );
