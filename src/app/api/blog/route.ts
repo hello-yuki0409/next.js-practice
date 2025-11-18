4124d1ae7157458a045299c5460046a5924ccf73
hello-yuki0409 <bobu2784@gmail.com>
2025-11-18 22:48:19 +0900

    fix: app/apiディレクトリに書き換え

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@/src/utils/supabaseClient";
+import { NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, res: NextApiResponse) {
+  const { data, error } = await supabase.from("posts").select("*");
+
+  if (error) {
+    return NextResponse.json(error);
+  }
+
+  return NextResponse.json(data, { status: 200 });
+}
