6096c268c4b44633c1aae2bd45b2fc0a10ce93fc
hello-yuki0409 <bobu2784@gmail.com>
2025-11-19 23:35:34 +0900

    fix: 詳細記事用api書き換え

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -5,7 +5,7 @@ import DeleteButton from "../../components/DeleteButton";
 const Article = async ({ params }: { params: { id: string } }) => {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-  const res = await fetch(`${API_URL}/api/${params.id}`, { next: { revalidate: 10 } });
+  const res = await fetch(`${API_URL}/api/blog/${params.id}`, { next: { revalidate: 10 } });
   const detailArticle = await res.json();
 
   return (
